d6d496d3922ed3ee806c22e94a5ef95bfbda8e25
ChanceCafferty <chance.r.cafferty@gmail.com>
2022-06-09 12:50:41 -0500

    uhh 3

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req,res) => {
-    res.send('<h1>Hello, world!<h1>')
+    res.send('<h1>Hello, world! - Chance C.<h1>')
 })
 
 module.exports = server;
